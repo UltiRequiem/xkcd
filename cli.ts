6bfc6baeb795086481ed5328c686921c2af2d3ab
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-04 09:58:47 -0500

    :sparkles: Add spinner

--- cli.ts
+++ cli.ts
@@ -1,12 +1,12 @@
 import xkdc from "./mod.ts";
-import { Args, ensureDir, parse } from "./deps.ts";
+import { Args, ensureDir, parse, Spinner } from "./deps.ts";
 import { download, filenameFromUrl } from "./utils.ts";
 
-const COMMAND = "xkdc";
+const APP_NAME = "xkcd";
 
 function showHelp() {
   console.log(`
-    Usage: ${COMMAND} [OPTIONS]
+    Usage: ${APP_NAME} [OPTIONS]
 
     Options:
       -h, --help       Show this help message
@@ -17,7 +17,7 @@ function showHelp() {
 }
 
 function showVersion() {
-  console.log(`${COMMAND} v0.1.0`);
+  console.log(`${APP_NAME} v0.1.0`);
   Deno.exit(0);
 }
 
@@ -33,12 +33,16 @@ function checkForNotMultipleArguments(args: Args) {
   }
 }
 
+function formatPath(dir: string, num: number, filename: string) {
+  return `${dir}/${num}_${filenameFromUrl(filename)}`;
+}
+
 async function main() {
   const args = parse(Deno.args);
 
   checkForNotMultipleArguments(args);
 
-  const { help, dir = "./xkdc_data", version } = args;
+  const { help, dir = `./${APP_NAME}_data`, version, all } = args;
 
   if (help) showHelp();
   if (version) showVersion();
@@ -46,7 +50,27 @@ async function main() {
   const { img, num } = await xkdc();
 
   await ensureDir(dir);
-  await download(img, `./${dir}/${num}_${filenameFromUrl(img)}`);
+
+  const spinner = Spinner.getInstance();
+
+  if (all) {
+    spinner.start(`Downloading ${num} comics...`);
+
+    const downloadPromises = Array.from({ length: num });
+
+    for (let index = 0; index < num; index++) {
+      if (index == 404) continue;
+      const { img, num } = await xkdc(index);
+      downloadPromises.push(await download(img, formatPath(dir, num, img)));
+    }
+
+    await Promise.all(downloadPromises);
+    await spinner.succeed(`${num} comics downloaded`);
+    return;
+  }
+
+  await download(img, filenameFromUrl(img));
+  await spinner.succeed("Latest comic downloaded");
 }
 
 if (import.meta.main) {
