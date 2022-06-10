5f147635d465d1163187fcd317b03c0bf5004bde
GitHub <noreply@github.com>
2022-06-10 00:14:07 +0000

    :arrow_up: Update Dependencies

--- cli_deps.ts
+++ cli_deps.ts
@@ -1,5 +1,5 @@
-export * from "https://deno.land/std@0.142.0/flags/mod.ts";
-export * from "https://deno.land/std@0.142.0/fs/mod.ts";
+export * from "https://deno.land/std@0.143.0/flags/mod.ts";
+export * from "https://deno.land/std@0.143.0/fs/mod.ts";
 export {
   default as Kia,
   Spinners,
