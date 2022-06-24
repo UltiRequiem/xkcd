1af9cc2135b357012bd01879d2746d5d9993b383
GitHub <noreply@github.com>
2022-06-24 00:14:10 +0000

    :arrow_up: Update Dependencies

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.144.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.145.0/testing/asserts.ts";
 import xkcd from "./mod.ts";
 
 Deno.test("xkcd", async () => {
