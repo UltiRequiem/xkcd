c0dffe21f44936b1cc648d39b5379ed2baaf85fe
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-19 16:24:11 -0500

    :bug: Bug

--- scripts/node.ts
+++ scripts/node.ts
@@ -1,7 +1,7 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.17/node.ts";
 
 buildPackage({
-  repoName: "xkcd",
+  repoName: "@ultirequiem/xkcd",
   description: "Interact with the XKCD API.",
   homepage: "https://ulti.js.org/xkcd",
   keywords: ["xkcd", "comic"],
