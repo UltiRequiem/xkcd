56ea0b65867a8799eea90b9193b6de9934f6e02a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-20 10:38:52 -0500

    :bookmark: 2.1.0

--- scripts/node.ts
+++ scripts/node.ts
@@ -1,10 +1,18 @@
 import { buildPackage } from "https://deno.land/x/ultirequiem@0.0.17/node.ts";
 
-buildPackage({
-  repoName: "@ultirequiem/xkcd",
-  description: "Interact with the XKCD API.",
-  homepage: "https://ulti.js.org/xkcd",
-  keywords: ["xkcd", "comic"],
-  license: "MIT",
-  version: "2.0.0",
-});
+buildPackage(
+  {
+    repoName: "@ultirequiem/xkcd",
+    description: "Interact with the XKCD API.",
+    homepage: "https://ulti.js.org/xkcd",
+    keywords: ["xkcd", "comic"],
+    license: "MIT",
+    version: "2.1.0",
+  },
+  {
+    entryPoints: ["./mod.ts", { name: "xkcd", path: "./xkcd.ts", kind: "bin" }],
+    typeCheck: false,
+    supportCJS: false,
+    shims: { deno: true, prompts: true },
+  },
+);
